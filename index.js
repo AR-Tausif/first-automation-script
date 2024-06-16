64f529dd3182e6007efe7ec394909a95384a0261
Tausif Ahmed <tausif.ritu1@gmail.com>
2024-06-16 16:01:29 +0600

    search result print on console successfully

--- index.js
+++ index.js
@@ -1,59 +1,34 @@
-import puppeteer from "puppeteer"; // or import puppeteer from 'puppeteer-core';
+import puppeteer from "puppeteer"; // ^22.10.0
 
-const run = async () => {
-  // Launch the browser and open a new blank page
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
+let browser;
+(async () => {
+  const queryText = "Abu taher web automation in bangladesh";
 
-  let query = "how to scrape google";
-  console.log("query :", query);
-  // Navigate the page to a URL.
-  await page.goto("https://www.google.com/search?q=" + query, {
-    waitUntil: "load",
-    timeout: 0,
+  browser = await puppeteer.launch();
+  const [page] = await browser.pages();
+  await page.setRequestInterception(true);
+  await page.setJavaScriptEnabled(false);
+  page.on("request", (request) => {
+    request.resourceType() === "document"
+      ? request.continue()
+      : request.abort();
   });
-
-  let numResult = await page.evaluate(() => {
-    let i = 0;
-    document.getElementById("rso").childNodes.forEach((element) => {
-      if (element.tagName == "DIV") {
-        i++;
-      }
-    });
-    return i;
+  await page.goto("https://www.google.com/", {
+    waitUntil: "domcontentloaded",
   });
-  console.log("result", numResult);
-
-  //   // Set screen size.
-  //   await page.setViewport({ width: 1080, height: 1024 });
-
-  //   // Type into search box.
-  //   await page.type(
-  //     ".devsite-search-field",
-  //     "Who is Abu Taher in Bangladesh automation developer?"
-  //   );
-
-  //   // Wait and click on first result.
-  //   const searchResultSelector = ".devsite-result-item-link";
-  //   await page.waitForSelector(searchResultSelector);
-  //   await page.click(searchResultSelector);
-
-  //   // Locate the full title with a unique string.
-  //   const textSelector = await page.waitForSelector(
-  //     "text/Customize and automate"
-  //   );
-  //   const fullTitle = await textSelector?.evaluate((el) => el.textContent);
-
-  //   // Print the full title.
-  //   console.log('The title of this blog post is "%s".', fullTitle);
-
-  //   let result = [];
-  //   for (let index = 1; index <= numResult; index++) {
-  //     const res = await page.select();
-  //     console.log(res);
-  //   }
-
-  await browser.close();
-};
-
-run().catch(console.error);
+  await page.type("textarea", queryText);
+  await page.$eval('[aria-label="Google Search"]', (el) => el.click());
+  const sel = ".Gx5Zad";
+  await page.waitForSelector(sel);
+  const searchResults = await page.$$eval(sel, (els) =>
+    els
+      .map((e) => ({
+        title: e.querySelector("h3")?.textContent,
+        link: e.querySelector("a").href,
+      }))
+      .filter((e) => e.title)
+  );
+  console.log(searchResults);
+})()
+  .catch((err) => console.error(err))
+  .finally(() => browser?.close());
